1e9a2f33ca9c82cea41758c9257a3a95682d2606
Alberto <albertiae.digital@gmail.com>
2023-03-28 19:41:47 -0400

    Created index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+/*
+This file contains the HTML code for the basic calculator user interface. It includes
+a form with two input fields: one for the nubers to be calculated and another for the 
+operation to be performed (addition, substraction, multiplication, or division). It 
+also includes a link to the `style.css` file, which contains the CSS code for styling 
+the calculator.
+*/
