378be263ac049c69c564cdd3b4725fd750a764b8
Alberto Hernández <albertiae.digital@gmail.com>
2023-08-20 10:11:16 -0500

    Updated the logical procedures of the php calculator

--- php/calculate.php
+++ php/calculate.php
@@ -9,3 +9,48 @@ any of the numbers are zero, to avoid division by zero errors. If there are no e
 it displays the result in a paragraph element. If there are errors, it displays an
 error message instead.
 */
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Basic Calculator</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="calculator">
+		<h2>Basic Calculator</h2>
+		<?php
+		if (isset($_POST['numbers']) && isset($_POST['operation'])) {
+			$numbers = explode(',', $_POST['numbers']);
+			$operation = $_POST['operation'];
+			$result = 0;
+			switch ($operation) {
+				case 'add':
+					$result = array_sum($numbers);
+					break;
+				case 'subtract':
+					$result = $numbers[0];
+					for ($i = 1; $i < count($numbers); $i++) {
+						$result -= $numbers[$i];
+					}
+					break;
+				case 'multiply':
+					$result = $numbers[0];
+					for ($i = 1; $i < count($numbers); $i++) {
+						$result *= $numbers[$i];
+					}
+					break;
+				case 'divide':
+					$result = $numbers[0];
+					for ($i = 1; $i < count($numbers); $i++) {
+						if ($numbers[$i] != 0) {
+							$result /= $numbers[$i];
+						} else {
+							echo '<p class="error">Division by zero!</p>';
+							break 2;
+						}
+					}
+					break;
+				default:
+					echo '<p class="error">Invalid operation!</p>';
+					break
